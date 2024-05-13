3da6888cf25494d392c876b826a9339cf613cd7b
Abhishek jat <abhishekjat80094@gmail.com>
2024-05-14 01:09:43 +0530

    added login logout functions

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import cors from 'cors'
 const app = express(); 
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser());
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 
